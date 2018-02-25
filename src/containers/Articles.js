955a0051f6ef455354bef25809183aad434b0f18
Jair Avilés <hi@jairaviles.mx>
2018-02-24 18:31:25 -0600

    fix minor issues

--- src/containers/Articles.js
+++ src/containers/Articles.js
@@ -41,7 +41,7 @@ class Articles extends Component {
               </div>
               <div className="card-content">
                 <span className="card-title activator grey-text text-darken-4 truncate">{article.title}<i className="material-icons right">more_vert</i></span>
-                <p><a href={article.url}>See more</a></p>
+                <p><a href={article.url} target="_blank">See more</a></p>
               </div>
               <div className="card-reveal">
                 <span className="card-title grey-text text-darken-4">{article.title}<i className="material-icons right">close</i></span>
